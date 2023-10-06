2897fe968531dff51c1da833ff97c9ba833c0a2c
jasin23 <jasinchavarria2315@gmail.com>
2023-10-06 06:42:56 -0600

    Semana 4 práctica 2, Jasin Chavarría V

---
+++ src/main/java/com/tiendaTelefonos/TiendaTelefonosApplication.java
@@ -0,0 +1,13 @@
+package com.tiendaTelefonos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaTelefonosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaTelefonosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tiendaTelefonos/TiendaTelefonosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiendaTelefonos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaTelefonosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
